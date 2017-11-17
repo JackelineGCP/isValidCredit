44f5a12a547463c52473df212444f804b1c84708
JackelineGCP <gessenia_17@hotmail.com>
2017-11-16 23:14:23 -0500

    sube mejoras en readme

--- js/app.js
+++ js/app.js
@@ -2,14 +2,14 @@
 //1.Crear función isValidCard
 
 function isValidCard(sixteenDigitString) {
-    parseInt(prompt('ingrese el numero de su tarjeta de crédito'));
+    var creditNumber = parseInt(prompt('ingrese número de su tarjeta de crédito'));
+  //  for(i = 0; i <= creditNumber.length; i++)
     var reverse = 0; //Donde se va a guardar los números invertidos
     var resto = sixteenDigitString; //Número de la tarjeta
     do {
        reverse = reverse * 10 + (resto % 10);
        resto = Math.floor(resto / 10);
       } while ( resto > 0 )
-      // {        return reverse};
 
     var numSum = 0; //numSum es la sumatoria de todos los números una vez apliacado las operaciones
     var value = 0; //value son el resultado de las operaciones que se les ha aplicado a los números (par, impar)
@@ -20,7 +20,7 @@ function isValidCard(sixteenDigitString) {
                 value = (Math.floor(value / 10) + value % 10);
             }
         } else {
-            value = +reverse[i];
+            value += reverse[i];
         }
         numSum += value;
     }
